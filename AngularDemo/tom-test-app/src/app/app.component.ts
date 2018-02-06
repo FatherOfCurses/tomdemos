20ea77976ac6d076f2363c5f56656eac178e0b3f
Colin Principe <colin.principe@libertymutual.com>
2018-02-06 14:25:15 -0500

    Adding Angular app

---
+++ AngularDemo/tom-test-app/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+// interface describes what the Greeting called below will look like 
+
+interface GreetingResponse {
+  id: number;
+  content: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  title = 'app';
+  // store private response variable that has a type of Greeting response
+  results: GreetingResponse;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    // call http client get method. Pass in URL, return observable that you subscribe to,
+    // then when data comes back, use arrow function to set data to the results variable declared above 
+    this.http.get <GreetingResponse>('http://localhost:8090/greeting').subscribe(data => {
+      this.results = data;
+    });
+  }
+
+}
