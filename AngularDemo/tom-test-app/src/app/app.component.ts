f44a7f85d0c27eaf921eeed32375406535bf7128
Colin Principe <colin.principe@libertymutual.com>
2018-02-08 11:07:41 -0500

    Replaced backend app with sandbox app

--- AngularDemo/tom-test-app/src/app/app.component.ts
+++ AngularDemo/tom-test-app/src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     // call http client get method. Pass in URL, return observable that you subscribe to,
     // then when data comes back, use arrow function to set data to the results variable declared above 
-    this.http.get <GreetingResponse>('http://localhost:8090/greeting').subscribe(data => {
+    this.http.get <GreetingResponse>('https://principetomdemo.sandbox.paas.lmig.com/greeting').subscribe(data => {
       this.results = data;
     });
   }
